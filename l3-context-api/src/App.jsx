d560699bf089ef27e1323f2bf5d31a356d59c27e
Mr-Explorer142 <tahmidalamtamim142@gmail.com>
2025-08-20 15:21:45 +0600

    Add Context API and useReducer functionality to TODO App

--- l3-context-api/src/App.jsx
+++ l3-context-api/src/App.jsx
@@ -4,51 +4,18 @@ import TodoItems from "./components/TodoItems";
 import WelcomeMessage from "./components/WelcomeMessage";
 import "./store/todo-items-store";
 import "./App.css";
-import { useState } from "react";
-import { TodoItemsContext } from "./store/todo-items-store";
+import TodoItemsContextProvider from "./store/todo-items-store";
 
 function App() {
-  // const initialTodoItems = [
-  //   { name: "React Coding", dueDate: "01/01/25" },
-  //   { name: "Book Reading", dueDate: "02/01/25" },
-  // ];
-
-  const [todoItems, setTodoItems] = useState([]);
-
-  const addNewItem = (itemName, itemDueDate) => {
-    // console.log(`New Item Added: ${itemName} Date:${itemDueDate}`);
-    // const newTodoItems = [
-    //   ...todoItems,
-    //   { name: itemName, dueDate: itemDueDate },
-    // ];
-    // setTodoItems(newTodoItems);
-    setTodoItems((currentValue) => [
-      ...currentValue,
-      { name: itemName, dueDate: itemDueDate },
-    ]);
-  };
-
-  const deleteItem = (todoItemName) => {
-    const newTodoItems = todoItems.filter((item) => item.name !== todoItemName);
-    setTodoItems(newTodoItems);
-    // console.log(`Item deleted : ${todoItemName}`);
-  };
-
   return (
-    <TodoItemsContext.Provider
-      value={{
-        todoItems: todoItems,
-        addNewItem: addNewItem,
-        deleteItem: deleteItem,
-      }}
-    >
+    <TodoItemsContextProvider>
       <center className="todo-container p-5">
         <AppName />
         <AddTodo />
         <WelcomeMessage></WelcomeMessage>
         <TodoItems></TodoItems>
       </center>
-    </TodoItemsContext.Provider>
+    </TodoItemsContextProvider>
   );
 }
 
--- l3-context-api/src/store/todo-items-store.jsx
+++ l3-context-api/src/store/todo-items-store.jsx
@@ -1,6 +1,63 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
+
+
 export const TodoItemsContext = createContext({
   todoItems: [],
   addNewItem: () => {},
   deleteItem: () => {},
 });
+
+
+// Reducer function -> It is called a pure function. Because it is not inside the App() function
+const todoItemsReducer = (currTodoItem, action) => {
+  let newTodoItems = currTodoItem;
+  if (action.type === "NEW_ITEM") {
+    newTodoItems = [
+      ...currTodoItem,
+      { name: action.payload.itemName, dueDate: action.payload.itemDueDate },
+    ];
+  } else if (action.type === "DELETE_ITEM") {
+    newTodoItems = currTodoItem.filter(
+      (item) => item.name !== action.payload.itemName
+    );
+  }
+  return newTodoItems;
+};
+
+const TodoItemsContextProvider = ({children}) => {
+  const [todoItems, dispatchTodoItems] = useReducer(todoItemsReducer, []);
+
+  // Actions and Dispatchs
+  const addNewItem = (itemName, itemDueDate) => {
+    const newItemAction = {
+      type: "NEW_ITEM",
+      payload: {
+        itemName,
+        itemDueDate,
+      },
+    };
+    dispatchTodoItems(newItemAction);
+  };
+
+  const deleteItem = (itemName) => {
+    const deleteItemAction = {
+      type: "DELETE_ITEM",
+      payload: {
+        itemName,
+      },
+    };
+    dispatchTodoItems(deleteItemAction);
+  };
+
+  return <TodoItemsContext.Provider
+      value={{
+        todoItems: todoItems,
+        addNewItem: addNewItem,
+        deleteItem: deleteItem,
+      }} 
+    >
+      {children}
+    </TodoItemsContext.Provider>
+};
+
+export default TodoItemsContextProvider;
